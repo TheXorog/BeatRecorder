dbdd36e1642051935ad06fd6144ece1bd6810f95
XorogVEVO <xorog@outlook.de>
2021-10-25 21:17:39 +0200

    Changed to Queue instead of List

--- OBSControl/_logger.cs
+++ OBSControl/_logger.cs
@@ -154,7 +154,7 @@ namespace OBSControl
                                     break;
                             }
 
-                            Objects.LogsToPost.Remove(b);
+                            Objects.LogsToPost.Dequeue();
                         }
                     }
                     catch (Exception)
@@ -170,7 +170,7 @@ namespace OBSControl
         {
             TotalLogs++;
 
-            Objects.LogsToPost.Add(new Objects.LogEntry
+            Objects.LogsToPost.Enqueue(new Objects.LogEntry
             {
                 TimeOfEvent = DateTime.UtcNow,
                 LogLevel = 4,
@@ -183,7 +183,7 @@ namespace OBSControl
         {
             TotalLogs++;
             
-            Objects.LogsToPost.Add(new Objects.LogEntry
+            Objects.LogsToPost.Enqueue(new Objects.LogEntry
             {
                 TimeOfEvent = DateTime.UtcNow,
                 LogLevel = 3,
@@ -196,7 +196,7 @@ namespace OBSControl
         {
             TotalLogs++;
             
-            Objects.LogsToPost.Add(new Objects.LogEntry
+            Objects.LogsToPost.Enqueue(new Objects.LogEntry
             {
                 TimeOfEvent = DateTime.UtcNow,
                 LogLevel = 2,
@@ -209,7 +209,7 @@ namespace OBSControl
         {
             TotalLogs++;
 
-            Objects.LogsToPost.Add(new Objects.LogEntry
+            Objects.LogsToPost.Enqueue(new Objects.LogEntry
             {
                 TimeOfEvent = DateTime.UtcNow,
                 LogLevel = 1,
@@ -222,7 +222,7 @@ namespace OBSControl
         {
             TotalLogs++;
 
-            Objects.LogsToPost.Add(new Objects.LogEntry
+            Objects.LogsToPost.Enqueue(new Objects.LogEntry
             {
                 TimeOfEvent = DateTime.UtcNow,
                 LogLevel = 0,
