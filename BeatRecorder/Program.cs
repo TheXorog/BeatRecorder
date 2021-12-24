c4978bba1e539c9253c970be5d67dda796970ca0
XorogVEVO <xorog@outlook.de>
2021-12-25 00:18:51 +0100

    Update Program.cs

--- BeatRecorder/Program.cs
+++ BeatRecorder/Program.cs
@@ -25,7 +25,7 @@ namespace BeatRecorder
 {
     class Program
     {
-        public static string CurrentVersion = "1.5.1";
+        public static string CurrentVersion = "1.5.2";
 
         internal static WebsocketClient beatSaberWebSocket { get; set; }
         internal static WebsocketClient beatSaberWebSocketLiveData { get; set; }
