43dfd8cce240678cb2317a15a32a58ce145fdae9
Mira <56395159+TheXorog@users.noreply.github.com>
2022-08-25 01:18:54 +0200

    Null Checks in Update Method

--- BeatRecorder/Entities/SharedStatus.cs
+++ BeatRecorder/Entities/SharedStatus.cs
@@ -50,33 +50,33 @@ internal class SharedStatus
         if (GameInfo.ModUsed != newStatus.GameInfo.ModUsed)
             throw new InvalidOperationException("Mod used cannot be updated.");
 
-        GameInfo.ModVersion = newStatus.GameInfo.ModVersion;
-        GameInfo.GameVersion = newStatus.GameInfo.GameVersion;
+        GameInfo.ModVersion = newStatus.GameInfo.ModVersion ?? GameInfo.ModVersion;
+        GameInfo.GameVersion = newStatus.GameInfo.GameVersion ?? GameInfo.GameVersion;
 
-        BeatmapInfo.Name = newStatus.BeatmapInfo.Name;
-        BeatmapInfo.SubName = newStatus.BeatmapInfo.SubName;
-        BeatmapInfo.Author = newStatus.BeatmapInfo.Author;
-        BeatmapInfo.Creator = newStatus.BeatmapInfo.Creator;
-        BeatmapInfo.Cover = newStatus.BeatmapInfo.Cover;
-        BeatmapInfo.Bpm = newStatus.BeatmapInfo.Bpm;
-        BeatmapInfo.NoteJumpSpeed = newStatus.BeatmapInfo.NoteJumpSpeed;
-        BeatmapInfo.Difficulty = newStatus.BeatmapInfo.Difficulty;
-        BeatmapInfo.NoteCount = newStatus.BeatmapInfo.NoteCount;
-        BeatmapInfo.BombCount = newStatus.BeatmapInfo.BombCount;
-        BeatmapInfo.WallCount = newStatus.BeatmapInfo.WallCount;
-        BeatmapInfo.CustomDifficulty = newStatus.BeatmapInfo.CustomDifficulty;
+        BeatmapInfo.Name = newStatus.BeatmapInfo.Name ?? BeatmapInfo.Name;
+        BeatmapInfo.SubName = newStatus.BeatmapInfo.SubName ?? BeatmapInfo.SubName;
+        BeatmapInfo.Author = newStatus.BeatmapInfo.Author ?? BeatmapInfo.Author;
+        BeatmapInfo.Creator = newStatus.BeatmapInfo.Creator ?? BeatmapInfo.Creator;
+        BeatmapInfo.Cover = newStatus.BeatmapInfo.Cover ?? BeatmapInfo.Cover;
+        BeatmapInfo.Bpm = newStatus.BeatmapInfo.Bpm ?? BeatmapInfo.Bpm;
+        BeatmapInfo.NoteJumpSpeed = newStatus.BeatmapInfo.NoteJumpSpeed ?? BeatmapInfo.NoteJumpSpeed;
+        BeatmapInfo.Difficulty = newStatus.BeatmapInfo.Difficulty ?? BeatmapInfo.Difficulty;
+        BeatmapInfo.NoteCount = newStatus.BeatmapInfo.NoteCount ?? BeatmapInfo.NoteCount;
+        BeatmapInfo.BombCount = newStatus.BeatmapInfo.BombCount ?? BeatmapInfo.BombCount;
+        BeatmapInfo.WallCount = newStatus.BeatmapInfo.WallCount ?? BeatmapInfo.WallCount;
+        BeatmapInfo.CustomDifficulty = newStatus.BeatmapInfo.CustomDifficulty ?? BeatmapInfo.CustomDifficulty;
 
-        PerformanceInfo.RawScore = newStatus.PerformanceInfo.RawScore;
-        PerformanceInfo.Score = newStatus.PerformanceInfo.Score;
-        PerformanceInfo.Accuracy = newStatus.PerformanceInfo.Accuracy;
-        PerformanceInfo.Rank = newStatus.PerformanceInfo.Rank;
-        PerformanceInfo.MissedNoteCount = newStatus.PerformanceInfo.MissedNoteCount;
-        PerformanceInfo.BadCutCount = newStatus.PerformanceInfo.BadCutCount;
-        PerformanceInfo.BombHitCount = newStatus.PerformanceInfo.BombHitCount;
-        PerformanceInfo.MaxCombo = newStatus.PerformanceInfo.MaxCombo;
-        PerformanceInfo.SoftFailed = newStatus.PerformanceInfo.SoftFailed;
-        PerformanceInfo.Failed = newStatus.PerformanceInfo.Failed;
-        PerformanceInfo.Finished = newStatus.PerformanceInfo.Finished;
+        PerformanceInfo.RawScore = newStatus.PerformanceInfo.RawScore ?? PerformanceInfo.RawScore;
+        PerformanceInfo.Score = newStatus.PerformanceInfo.Score ?? PerformanceInfo.Score;
+        PerformanceInfo.Accuracy = newStatus.PerformanceInfo.Accuracy ?? PerformanceInfo.Accuracy;
+        PerformanceInfo.Rank = newStatus.PerformanceInfo.Rank ?? PerformanceInfo.Rank;
+        PerformanceInfo.MissedNoteCount = newStatus.PerformanceInfo.MissedNoteCount ?? PerformanceInfo.MissedNoteCount;
+        PerformanceInfo.BadCutCount = newStatus.PerformanceInfo.BadCutCount ?? PerformanceInfo.BadCutCount;
+        PerformanceInfo.BombHitCount = newStatus.PerformanceInfo.BombHitCount ?? PerformanceInfo.BombHitCount;
+        PerformanceInfo.MaxCombo = newStatus.PerformanceInfo.MaxCombo ?? PerformanceInfo.MaxCombo;
+        PerformanceInfo.SoftFailed = newStatus.PerformanceInfo.SoftFailed ?? PerformanceInfo.SoftFailed;
+        PerformanceInfo.Failed = newStatus.PerformanceInfo.Failed ?? PerformanceInfo.Failed;
+        PerformanceInfo.Finished = newStatus.PerformanceInfo.Finished ?? PerformanceInfo.Finished;
     }
 
     public SharedStatus Clone()
@@ -134,30 +134,30 @@ internal class SharedStatus
         internal string Creator { get; set; }
         internal Bitmap Cover { get; set; }
         internal string IdOrHash { get; set; }
-        internal float Bpm { get; set; }
-        internal float NoteJumpSpeed { get; set; }
+        internal float? Bpm { get; set; }
+        internal float? NoteJumpSpeed { get; set; }
         internal string Difficulty { get; set; }
         internal string CustomDifficulty { get => (CustomDifficulty.IsNullOrWhiteSpace() ? Difficulty : _CustomDifficulty); set { _CustomDifficulty = value; } }
-        internal long NoteCount { get; set; }
-        internal long BombCount { get; set; }
-        internal long WallCount { get; set; }
+        internal long? NoteCount { get; set; }
+        internal long? BombCount { get; set; }
+        internal long? WallCount { get; set; }
 
         private string _CustomDifficulty { get; set; }
     }
 
     internal class Performance
     {
-        internal long RawScore { get; set; }
-        internal long Score { get; set; }
-        internal double Accuracy { get; set; }
+        internal long? RawScore { get; set; }
+        internal long? Score { get; set; }
+        internal double? Accuracy { get; set; }
         internal string Rank { get; set; }
-        internal long MissedNoteCount { get; set; } = 0;
-        internal long BadCutCount { get; set; } = 0;
-        internal long BombHitCount { get; set; } = 0;
-        internal long CombinedMisses { get => MissedNoteCount + BadCutCount + BombHitCount; }
-        internal long MaxCombo { get; set; }
-        internal bool SoftFailed { get; set; }
-        internal bool Failed { get; set; }
-        internal bool Finished { get; set; }
+        internal long? MissedNoteCount { get; set; } = 0;
+        internal long? BadCutCount { get; set; } = 0;
+        internal long? BombHitCount { get; set; } = 0;
+        internal long? CombinedMisses { get => MissedNoteCount + BadCutCount + BombHitCount; }
+        internal long? MaxCombo { get; set; }
+        internal bool? SoftFailed { get; set; }
+        internal bool? Failed { get; set; }
+        internal bool? Finished { get; set; }
     }
 }
